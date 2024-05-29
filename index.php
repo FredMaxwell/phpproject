0f33aa7adcf13994a6e96d73da4ab3fc46f54bcd
Fred Maxwell <iloveetwix@yandex.ru>
2024-05-30 02:19:55 +0300

    first php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+var_dump($_REQUEST);
+
+?>
+
+<pre style="background-color: aqua"; color: red>
+<?php
+echo "Hello World!";
+?>
+    <li>
+        <ul>1</ul>
+        <ul>1</ul>
+        <ul>1</ul>
+        <ul>1</ul>
+    </li>
+</pre>
